4faa0828bdd410ca520e442a5cb6bc7812d32ee3
Emil215p <juelemil@gmail.com>
2023-11-28 10:49:31 +0100

    Image improvements, attempted UI improvements.

--- MAUICHAT/ChatPage.xaml.cs
+++ MAUICHAT/ChatPage.xaml.cs
@@ -2,14 +2,43 @@ namespace MAUICHAT;
 
 public partial class ChatPage : ContentPage
 {
+    public async Task<FileResult> PickAndShow(PickOptions options)
+    {
+        try
+        {
+            var result = await FilePicker.Default.PickAsync(options);
+            if (result != null)
+            {
+                if (result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
+                    result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
+                {
+                    using var stream = await result.OpenReadAsync();
+                    var image = ImageSource.FromStream(() => stream);
+                }
+            }
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            // The user canceled or something went wrong
+        }
+
+        return null;
+    }
+
     public ChatPage()
     {
         InitializeComponent();
     }
 
     private void OnSendMessage(object sender, EventArgs e)
-    { 
-    
+    {
+
+    }
+    private async void OnAttachMedia(object sender, EventArgs e)
+    {
+        await PickAndShow(PickOptions.Default);
     }
 
     private void OnReturnMenu(object sender, EventArgs e)
---
+++ MAUICHAT/Login.xaml.cs
@@ -0,0 +1,9 @@
+namespace MAUICHAT;
+
+public partial class Login : ContentPage
+{
+    public Login()
+    {
+        InitializeComponent();
+    }
+}
