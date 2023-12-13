5f7eb532af7fc0b1b77263adf9071f7305f5e4cf
Emil215p <juelemil@gmail.com>
2023-12-13 12:20:49 +0100

    Latest changes

--- MAUICHAT/ChatPage.xaml.cs
+++ MAUICHAT/ChatPage.xaml.cs
@@ -1,10 +1,50 @@
 using System.Text.Json;
 using System.Text;
 using System.Net.Http;
+using Microsoft.Maui.Controls;
+using System.ComponentModel;
 namespace MAUICHAT;
 
 public partial class ChatPage : ContentPage
 {
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        private string? _editorContent;
+        public string? EditorContent
+        {
+            get { return _editorContent; }
+            set
+            {
+                if (_editorContent != value)
+                {
+                    _editorContent = value;
+                    OnPropertyChanged(nameof(EditorContent));
+                }
+            }
+        }
+        private string? _nameContent;
+        public string? NameContent
+        {
+            get { return _nameContent; }
+            set
+            {
+                if (_nameContent != value)
+                {
+                    _nameContent = value;
+                    OnPropertyChanged(nameof(NameContent));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
     public async Task<FileResult?> PickAndShow(PickOptions options)
     {
         try
@@ -38,11 +78,13 @@ public partial class ChatPage : ContentPage
     {
         InitializeComponent();
     }
-
     private async void OnSendMessage(object sender, EventArgs e)
     {
+            string user = namebox.Text;
+            string? imageload = null;
+            string? editorcontent = editor.Text;
             var client = new HttpClient();
-            var data = new { username = "Emil", message = "Heello" }; // Data to send, use for testing.
+            var data = new { username = user, message = editorcontent, image = imageload}; // Data to send, use for testing.
             var json = JsonSerializer.Serialize(data); // Make the data Jason
             var content = new StringContent(json, Encoding.UTF8, "application/json"); // More Jason stuff
             await client.PostAsync("http://emko01.skp-dp.sde.dk/CSharpAPI_Test/index.php", content); // Post the data, declare API url
